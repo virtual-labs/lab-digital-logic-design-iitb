7feb7eeeffaa115ab04b5478cda52720e9cc39e5
vleadadmin <admin@vlabs.ac.in>
2021-12-01 09:35:56 +0000

    Pushing updated sources to Git

--- build/js/toggleSidebar.js
+++ build/js/toggleSidebar.js
@@ -1,19 +1,37 @@
+const sidebar = document.querySelector(".sidebar");
+const myModal = new bootstrap.Modal(document.getElementById("popupMenu"));
+const breakpointLg = 992;
+
 function toggle() {
-  const sb = document.querySelector(".sidebar");
-  if(sb.classList.contains("hidden")) {
-    sb.classList.remove("hidden");
-    sb.classList.add("visible");
-  }
-  else {
-    sb.classList.remove("visible");
-    sb.classList.add("hidden");
+  const w = $(this).width();
+  if (w < breakpointLg) {
+    myModal.toggle();
+  } else {
+    if (sidebar.classList.contains("vlabs-hidden")) {
+      sidebar.classList.remove("vlabs-hidden");
+    } else {
+      sidebar.classList.add("vlabs-hidden");
+    }
   }
 }
 
-$(window).resize(() => {
-  const w = $(this).width();
-  if (w < 576) {
-    document.querySelector(".sidebar").classList.remove("visible");
-    document.querySelector(".sidebar").classList.add("hidden");
+function simulationHeaderToggle() {
+  const dropdown = document.querySelector(
+    ".simulation-header .dropdown .backdrop"
+  );
+  if (dropdown.classList.contains("vlabs-hidden")) {
+    dropdown.classList.remove("vlabs-hidden");
+  } else {
+    dropdown.classList.add("vlabs-hidden");
   }
-});
+}
+
+if (document.querySelector(".simulation-header")) {
+  window.addEventListener("click", ({ target }) => {
+    if (!(target.closest(".navbar-toggler") || target.closest(".nav-menu"))) {
+      document
+        .querySelector(".simulation-header .dropdown .backdrop")
+        .classList.add("vlabs-hidden");
+    }
+  });
+}
